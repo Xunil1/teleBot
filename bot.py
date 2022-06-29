d637bd148f23ff1a5868c0e176f02849c1593722
Xunil1 <ka.po.po@yandex.ru>
2022-06-30 02:23:43 +0500

    Start

--- bot.py
+++ bot.py
@@ -226,22 +226,6 @@ def callback_inline(call):
         print(repr(e))
 
 
-def send_message(time_out):
-    last_time = time.time()
-    while True:
-        if time.time() - last_time > time_out:
-
-            file = open("id.txt", "r")
-            chat_id = file.read()
-            file.close()
-            orders = requests.get("https://hookah-batumi.herokuapp.com/set_orders/5000").json()
-            if orders:
-                for el in orders:
-                    message = "Номер заказа: #" + str(orders[el]["id"]) + "\nЗаказ: " + str(orders[el]["order_el"]) + "\nИмя: " + str(orders[el]["name"]) + "\nАдрес: " + str(orders[el]["address"]) + "\nТелефон: " + str(orders[el]["phone"]) + "\nМессенджер: " + str(orders[el]["messenger"]) + "\nКомментарий: " + str(orders[el]["comment"]) + "\nОбщая стоимость: " + str(orders[el]["order_price"])
-                    bot.send_message(int(chat_id), message)
-
-            last_time = time.time()
-
 def get_product():
     products = requests.get("https://hookah-batumi.herokuapp.com/set_products").json()
     print(products)
